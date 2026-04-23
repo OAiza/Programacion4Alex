b68b6531b93b2a89251c3cab95105193e0353f4c
OAiza <power777pro@gmai.com>
2026-04-23 14:35:36 -0600

    Re type router route

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
-import { router } from './router'
-
+import {router} from './Router'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
